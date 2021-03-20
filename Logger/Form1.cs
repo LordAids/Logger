31829e5b656201d300e0eb5746cbe9265b06613f
LordAids <62069378+LordAids@users.noreply.github.com>
2021-03-20 20:28:05 +0300

    Initial commit

---
+++ Logger/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logger
+{
+    public partial class Form1 : Form
+    {
+        Logger log = new Logger();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            log.Debug("Была нажата кнопка дебага");
+            label1.Text = sender.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void debugEButton_Click(object sender, EventArgs e)
+        {
+            Exception ex = new Exception("Ошибка для показа debugE");
+            log.Debug("Была нажата кнопка дебага и вот код ошибки: ", ex);
+        }
+    }
+}
---
+++ Logger/Logger.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    class Logger : ILog
+    {
+        string dateNow = DateTime.Now.ToString();
+        string logFolder = Environment.CurrentDirectory + @"\log";
+        public void Debug(string message)
+        {
+            string dateDay = DateTime.Today.ToShortDateString();            
+
+            if (!Directory.Exists(logFolder + @"\" + dateDay)) Directory.CreateDirectory(logFolder + @"\" + dateDay);
+
+            StreamWriter sw = new StreamWriter(logFolder+ @"\" + dateDay+@"\Debug.txt", true);
+            sw.WriteLine(dateNow + " : " + message);
+            sw.Close();
+        }
+
+        public void Debug(string message, Exception e)
+        {
+            string dateDay = DateTime.Today.ToShortDateString();
+
+            if (!Directory.Exists(logFolder + @"\" + dateDay)) Directory.CreateDirectory(logFolder + @"\" + dateDay);
+
+            StreamWriter sw = new StreamWriter(logFolder + @"\" + dateDay + @"\Debug.txt", true);
+            sw.WriteLine(dateNow + " : " + message + e.ToString());
+            sw.Close();
+        }
+
+        public void DebugFormat(string message, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message, Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorUnique(string message, Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(string message, Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string message, Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string message, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SystemInfo(string message, Dictionary<object, object> properties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warning(string message, Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WarningUnique(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
